12c1a0fce12df890ec6877c28c6d9582b2f467dd
Steven Lacks <snlacks@gmail.com>
2018-10-23 21:08:31 -0400

    adds units tests for POST Users

--- src/routes/api1_0/users.test.ts
+++ src/routes/api1_0/users.test.ts
@@ -23,6 +23,64 @@ describe('Users', () => {
         await request(server)
             .get('/api.v1.0/users')
             .expect(200);
-    })
+    });
+    it('should post with valid user', async() => {
+        await request(server)
+            .post('/api.v1.0/users')
+            .send({
+                name: "Mack",
+                email: "francis1234@gmail.com",
+                password: "1234abcdABCD"
+            })
+            .expect(201);
+    });
+    it('should not post with invalid user name', async() => {
+        await request(server)
+            .post('/api.v1.0/users')
+            .send({
+                name: "M",
+                email: "francis1234@gmail.com",
+                password: "1234abcdABCD"
+            })
+            .expect(400);
+    });
+    it('should not post with invalid email', async() => {
+        await request(server)
+            .post('/api.v1.0/users')
+            .send({
+                name: "Mack",
+                email: "francis1234@com",
+                password: "1234abcdABCD"
+            })
+            .expect(400);
+    });
+    it('should not post with invalid passwords', async() => {
+        await request(server)
+            .post('/api.v1.0/users')
+            .send({
+                name: "Mack",
+                email: "francis1234@gmail.com",
+                password: "1234abcd"
+            })
+            .expect(400);
+
+        await request(server)
+            .post('/api.v1.0/users')
+            .send({
+                name: "Mack",
+                email: "francis1234@gmail.com",
+                password: "1234ABCD"
+            })
+            .expect(400);
+
+        await request(server)
+            .post('/api.v1.0/users')
+            .send({
+                name: "Mack",
+                email: "francis1234@gmail.com",
+                password: "123Abc!"
+            })
+            .expect(400);
+    });
 });
 
